501408f6d6b80f6d0a82bc34d3261dd05757e3f4
ernesto2011 <skillmx107@gmail.com>
2024-10-19 21:38:02 -0600

    :lipstick: add styles to posts

---
+++ src/config/site-config.ts
@@ -0,0 +1,22 @@
+export const siteConfig= {
+    title: "Ernesto Lopez",
+    description: "My personal website",
+    navItems: [
+        {
+            label: "Home",
+            href: "/"
+        },
+        {
+            label: "About",
+            href: "/about"
+        },
+        {
+            label: "Projects",
+            href: "/projects"
+        },
+        {
+            label: "Contact",
+            href: "/contact"
+        }
+    ]
+}
